d971981bfb487a9077a9a3bd0e552c47eb81baff
dzenan.dzafic <dzenan.dzafic@klika.ba>
2022-07-05 11:32:59 +0200

    Accessing the form data in the py file

--- serve.py
+++ serve.py
@@ -1,5 +1,7 @@
 from http.server import SimpleHTTPRequestHandler
 from socketserver import TCPServer
+from urllib.parse import parse_qs
+
 import os
 
 class RequestHandler(SimpleHTTPRequestHandler):
@@ -9,13 +11,13 @@ class RequestHandler(SimpleHTTPRequestHandler):
         return SimpleHTTPRequestHandler.do_GET(self)
     
     def do_POST(self):
-        content = int(self.headers.get("Content-Length"))
-        body = self.rfile.read(content)
-        print(body)
+        length = int(self.headers.get('Content-Length'))
+        postvars = parse_qs(self.rfile.read(length), keep_blank_values=1)
+        print(postvars)
         return self.do_GET()
 
 
-hostName, PORT = "localhost", 8082
+hostName, PORT = "localhost", 8001
 handler = RequestHandler
 
 server = TCPServer((hostName, PORT), handler)
